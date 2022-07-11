7e674aad189ba4e3f4ce5f69b9084e16ef9fecc9
Nishant Joshi <nishant.joshi@wingify.com>
2022-07-12 01:29:55 +0530

    Dockerfile added

--- utils.py
+++ utils.py
@@ -3,12 +3,12 @@ test = "The engineer went into the garden and found the cats lying beneath the t
 from textblob import TextBlob
 from collections import Counter
 from nltk.corpus import stopwords
-
+from string import punctuation
 def get_words(text):
     processor = TextBlob(text)
-    words_count = Counter((TextBlob(word.correct().stem()).correct().raw for word in processor.words))
+    words_count = Counter((word.correct().stem() for word in processor.words))
 
     return words_count
 
 def remove_stopwords(counter):
-    return {key: value for key, value in counter.items() if not key in stopwords.words()}
+    return {key: value for key, value in counter.items() if key not in stopwords.words() and key not in punctuation}
